276b28bba2c7020790a39c2d97f1073ab0e8127b
Suboptimal Engineer <suboptimaleng@gmail.com>
2025-07-05 13:33:07 -0400

    Set up code for 4th tutorial.

---
+++ 004-vscode-build-run/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  // int x = "abc";
+
+  int a = 101;
+  std::cout << "a -> " << a << std::endl;
+
+  int b{10};
+  std::cout << "b -> " << b << std::endl;
+
+  double d = 25.5;
+  std::cout << "d -> " << d << std::endl;
+
+  return 0;
+}
