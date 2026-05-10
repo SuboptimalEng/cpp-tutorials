980bad1bcb185c26a65204e9ab02c218da5936aa
Suboptimal Engineer <suboptimaleng@gmail.com>
2026-05-09 17:57:04 -0700

    Update code.

--- 036-threads-intro/main.cpp
+++ 036-threads-intro/main.cpp
@@ -8,20 +8,13 @@ void print_hello() {
   cout << "hello from thread!" << endl;
 }
 
-void fn() {}
-
 void add(int x, int y) {
   cout << x + y << endl;
 }
 
 int main() {
   // pthread, thread, jthread
-
   std::thread t1(print_hello);
-
-  // std::thread t1(fn);
-  // std::thread t2(fn);
-
   std::thread t2(add, 1, 2);
 
   cout << "in main" << endl;
