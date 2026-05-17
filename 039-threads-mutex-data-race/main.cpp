41e8566846fec5748679a88308ade2a59b0ec0b9
Suboptimal Engineer <suboptimaleng@gmail.com>
2026-05-17 13:34:23 -0700

    Update code.

--- 039-threads-mutex-data-race/main.cpp
+++ 039-threads-mutex-data-race/main.cpp
@@ -8,23 +8,29 @@ std::mutex mtx;
 int counter = 0;
 
 void increment() {
-  // cout << "counter: " << counter << endl;
-  for (size_t i = 0; i < 100000; i++) {
-    mtx.lock();
+  for (size_t i = 0; i < 10000; i++) {
+    // mtx.lock();
     counter++;
-    mtx.unlock();
+    // mtx.unlock();
   }
-  // cout << "counter: " << counter << endl;
 }
 
 int main() {
+  // example 1 - multiple lines of assembly
+  // std::thread t1(increment);
+  // std::thread t2(increment);
+  // t1.join();
+  // t2.join();
+  // cout << "counter: " << counter << endl;
+
+  // example 2 - 1 line of assembly
   for (int i = 0; i < 10000; i++) {
     counter = 0;
     std::thread t1(increment);
     std::thread t2(increment);
     t1.join();
     t2.join();
-    if (counter < 200000) {
+    if (counter < 20000) {
       cout << "counter: " << counter << endl;
     }
   }
