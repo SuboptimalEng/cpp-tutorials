c1757125d59e56a8f061ea9b96d12cd59a16e598
Suboptimal Engineer <suboptimaleng@gmail.com>
2026-07-31 22:26:06 -0700

    Update readme.

--- 047-compiler-optimizations/README.md
+++ 047-compiler-optimizations/README.md
@@ -2,8 +2,13 @@
 
 ## Titles
 
-- Intro to C++ Compiler Optimizations
+- I wrote a C++ loop. -O3 Deleted it.
+- Are You Smarter than the C++ Compiler Optimizer?
 - What's the difference between -O0 and -O3 C++ optimizations?
+
+- You vs -O3: Who Wins the C++ Speed Test?
+- Intro to C++ Compiler Optimizations
+- How -O3 Makes C++ Loops 100x Faster
 
 ## References
 
--- 047-compiler-optimizations/main.cpp
+++ 047-compiler-optimizations/main.cpp
@@ -25,13 +25,13 @@ int sumVector(std::vector<int>& v) {
 }
 
 int main(int argc) {
-  // int s1 = sum100();
-  // std::cout << "sum 100: " << s1 << std::endl;
+  int s1 = sum100();
+  std::cout << "sum 100: " << s1 << std::endl;
 
   // Dynamic size prevents constant folding (1000 elements)
-  std::vector<int> v(argc > 100 ? 10 : 1000, 5);
-  int s2 = sumVector(v);
-  std::cout << "sum vector: " << s2 << std::endl;
+  // std::vector<int> v(argc > 100 ? 10 : 1000, 5);
+  // int s2 = sumVector(v);
+  // std::cout << "sum vector: " << s2 << std::endl;
 
   return 0;
 }
